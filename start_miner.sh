f7a4adfed067932be1d9fdbc9022fc091792651a
Hollyqui <felix.quinque@gmail.com>
2025-08-11 12:37:12 +0100

    Deleting all files

--- start_miner.sh
+++
@@ -1,4 +0,0 @@
-cp .env src/miner/.env
-cd src/miner
-uv sync
-uv run --package miner main.py
--- start_validators.sh
+++
@@ -1,4 +0,0 @@
-cp .env src/validator/.env
-cd src/validator
-uv sync
-uv run --package validator main.py
